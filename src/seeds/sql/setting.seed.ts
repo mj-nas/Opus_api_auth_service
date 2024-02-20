b4a61e93b197f3bb2eb1263ddd0b922a90cd3521
sahil <sahil@newagesmb.com>
2024-02-20 15:17:15 +0530

    setting seed added

--- src/seeds/sql/setting.seed.ts
+++ src/seeds/sql/setting.seed.ts
@@ -12,14 +12,56 @@ export default <Seed<Setting>>{
       value: 'value 1',
       group_id: 1,
     },
-    
-      {
-        name: 'setting_1',
-        display_name: 'Setting 1',
-        value: 'value 1',
-        group_id: 1,
+
+    {
+      name: 'email',
+      display_name: 'Contact Email',
+      value: 'priya@newagesmb.com',
+      group_id: 1,
+      option: {
+        type: 'email',
+        format: 'email',
+        required: false,
+        min: 0,
       },
-    
+    },
+    {
+      name: 'shipping_limit',
+      display_name: 'Free shipping limit',
+      value: '70.00',
+      group_id: 1,
+      option: {
+        type: 'number',
+        format: 'amount',
+        required: false,
+        min: 0,
+      },
+    },
+    {
+      name: 'commission',
+      display_name: 'Commission',
+      value: '25',
+      group_id: 1,
+      option: {
+        type: 'number',
+        format: 'amount',
+        required: false,
+        min: 0,
+      },
+    },
+    {
+      name: 'minus_price',
+      display_name: ' Minus Price (Shipping)',
+      value: '25',
+      group_id: 1,
+      option: {
+        type: 'number',
+        format: 'amount',
+        required: false,
+        min: 0,
+      },
+    },
+
     {
       name: 'setting_2',
       display_name: 'Setting 2',
