8d62ea1d7b9ea515968f3bb53dba7e3966ab6f50
sahil <sahil@newagesmb.com>
2024-02-20 11:15:31 +0530

    templates put fixed

--- src/modules/sql/template/dto/update-template.dto.ts
+++ src/modules/sql/template/dto/update-template.dto.ts
@@ -2,5 +2,5 @@ import { OmitType, PartialType } from '@nestjs/swagger';
 import { Template } from '../entities/template.entity';
 
 export class UpdateTemplateDto extends PartialType(
-  OmitType(Template, ['name', 'active'] as const),
+  OmitType(Template, ['name'] as const),
 ) {}
