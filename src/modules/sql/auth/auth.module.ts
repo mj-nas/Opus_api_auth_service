84a5f90ef0413de3aa1d33d6bba846196c151e1e
sahil <sahil@newagesmb.com>
2024-02-21 14:30:10 +0530

    dependency error fixed

--- src/modules/sql/auth/auth.module.ts
+++ src/modules/sql/auth/auth.module.ts
@@ -9,6 +9,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt/jwt.strategy';
 import { TokenStrategy } from './strategies/token/token.strategy';
+import { MsClientModule } from 'src/core/modules/ms-client/ms-client.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TokenStrategy } from './strategies/token/token.strategy';
     OtpSessionModule,
     UserModule,
     CachingModule,
+    MsClientModule
   ],
   providers: [AuthService, JwtStrategy, TokenStrategy],
   controllers: [AuthController],
