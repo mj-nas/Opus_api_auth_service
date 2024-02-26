e01ed6b9413ff1ed64137c81566b433c34f58ffa
Krishnadas P <krish@newagesmb.com>
2024-02-27 01:06:00 +0530

    fix(dto): validation added

--- src/modules/sql/product-gallery/dto/create-presigned-url.dto.ts
+++ src/modules/sql/product-gallery/dto/create-presigned-url.dto.ts
@@ -1,13 +1,17 @@
-
 import { ApiProperty, OmitType } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { ProductGallery } from '../entities/product-gallery.entity';
 
-export class CreatePresignedUrl extends OmitType(ProductGallery, ['active','product_id','product_image','is_primary']) {
-  
+export class CreatePresignedUrl extends OmitType(ProductGallery, [
+  'active',
+  'product_id',
+  'product_image',
+  'is_primary',
+]) {
   @ApiProperty({
     description: 'key for creating presigned url',
-    example: "323434343434.png",
+    example: '323434343434.png',
   })
-  key:string;
-
+  @IsString()
+  key: string;
 }
