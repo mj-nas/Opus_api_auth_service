6f00d3bada867374ca7ee048ec2644e06b1361a7
Krishnadas P <krish@newagesmb.com>
2024-04-22 11:26:57 +0530

    change password by admin bug fix

--- src/modules/sql/user/dto/change-password-by-admin.dto.ts
+++ src/modules/sql/user/dto/change-password-by-admin.dto.ts
@@ -1,7 +1,13 @@
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 import { User } from '../entities/user.entity';
 
 export class ChangePasswordByAdminDto extends PickType(User, [
-  'id',
   'password',
-] as const) {}
+] as const) {
+  @ApiProperty({
+    format: 'int32',
+    description: 'User ID',
+    example: 1,
+  })
+  id: number;
+}
