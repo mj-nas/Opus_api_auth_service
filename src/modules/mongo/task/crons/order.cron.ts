f1f9edc24e66a217635c26e31f78be0befc578ee
Krishnadas P <krish@newagesmb.com>
2024-08-08 20:18:07 +0530

    cron

--- src/modules/mongo/task/crons/order.cron.ts
+++ src/modules/mongo/task/crons/order.cron.ts
@@ -13,7 +13,7 @@ export class OrderCron {
 
   // Reorder order
   // @Cron(CronExpression.EVERY_DAY_AT_6AM)
-  @Cron('46 14 * * *')
+  @Cron('52 14 * * *')
   async reorderCron() {
     // if (this.configService.get('appId') != 'crons') {
     //   return;
