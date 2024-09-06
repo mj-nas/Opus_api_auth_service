46eb073999eddfea763d2a0e7c30b6524148c1ac
mj <jabir@newagesysit.com>
2024-09-06 15:23:30 +0530

    refactor: Update unique_id generation logic in ExamModuleService

--- src/modules/sql/exam-module/exam-module.service.ts
+++ src/modules/sql/exam-module/exam-module.service.ts
@@ -67,7 +67,7 @@ export class ExamModuleService extends ModelService<ExamModule> {
         let unique_id = '';
         if (no_of_certs.data.length > 0) {
           const cert_id = no_of_certs.data[0].cert_id.split('-')[1];
-          unique_id = `OPUS-${parseInt(cert_id) + 1}`;
+          unique_id = `OPUS-${zeroPad((parseInt(cert_id) + 1).toString(), 5)}`;
         } else {
           unique_id = `OPUS-${zeroPad('1', 5)}`;
         }
