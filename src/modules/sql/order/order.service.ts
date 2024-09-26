3a2b6e9087cbd5806d39066e667123e2e43a7e8d
mj <jabir@newagesysit.com>
2024-09-26 17:04:32 +0530

    refactor(order): remove card number formatting in order creation email template

--- src/modules/sql/order/order.service.ts
+++ src/modules/sql/order/order.service.ts
@@ -508,12 +508,6 @@ export class OrderService extends ModelService<Order> {
         const shipping_address = `${body.address.shipping_first_name + ' ' + body.address.shipping_last_name}, ${body.address.shipping_address}, ${body.address.shipping_city}, ${body.address.shipping_state}, ${body.address.shipping_zip_code}`;
         const billing_address = `${body.address.billing_first_name + ' ' + body.address.billing_last_name}, ${body.address.billing_address}, ${body.address.billing_city}, ${body.address.billing_state}, ${body.address.billing_zip_code}`;
         // New order alert to admin for repeating order with card details
-
-        //convert card number into this format 1234 1234 1234 1234
-        const card_number = body.card_details.card_number.replace(
-          /(\d{4})/g,
-          '$1 ',
-        );
         await this._msClient.executeJob(
           'controller.notification',
           new Job({
