52e3c49e14ca71ce36bc1433c9fff645f0175fd5
mj <jabir@newagesysit.com>
2024-10-22 18:34:43 +0530

    xps remove try catch

--- libs/xps/src/xps.service.ts
+++ libs/xps/src/xps.service.ts
@@ -12,54 +12,49 @@ export class XpsService {
   ) {}
 
   async createShipment(job: Job): Promise<any> {
-    try {
-      const { payload } = job;
-      const senderData = await this._settingService.$db.getAllRecords({
-        options: {
-          where: {
-            group_id: 2,
-          },
-          attributes: ['name', 'value', 'display_name'],
+    const { payload } = job;
+    const senderData = await this._settingService.$db.getAllRecords({
+      options: {
+        where: {
+          group_id: 2,
         },
-      });
-      const senderObj = senderData.data.reduce((acc, curr) => {
-        acc[curr.display_name.toLowerCase()] = curr.value;
-        return acc;
-      }, {});
-      const returnAddressData = await this._settingService.$db.getAllRecords({
-        options: {
-          where: {
-            group_id: 3,
-          },
-          attributes: ['name', 'value', 'display_name'],
+        attributes: ['name', 'value', 'display_name'],
+      },
+    });
+    const senderObj = senderData.data.reduce((acc, curr) => {
+      acc[curr.display_name.toLowerCase()] = curr.value;
+      return acc;
+    }, {});
+    const returnAddressData = await this._settingService.$db.getAllRecords({
+      options: {
+        where: {
+          group_id: 3,
         },
-      });
-      const returnAddressObj = returnAddressData.data.reduce((acc, curr) => {
-        acc[curr.display_name.toLowerCase()] = curr.value;
-        return acc;
-      }, {});
-      payload.sender = senderObj;
-      payload.returnTo = returnAddressObj;
-      const apiKey = this._config.get('xps').api_key;
-      const customer_id = this._config.get('xps').customer_id;
-      const integration_id = this._config.get('xps').integration_id;
-      const url = `https://xpsshipper.com/restapi/v1/customers/${customer_id}/integrations/${integration_id}/orders/${payload.orderId}`;
+        attributes: ['name', 'value', 'display_name'],
+      },
+    });
+    const returnAddressObj = returnAddressData.data.reduce((acc, curr) => {
+      acc[curr.display_name.toLowerCase()] = curr.value;
+      return acc;
+    }, {});
+    payload.sender = senderObj;
+    payload.returnTo = returnAddressObj;
+    const apiKey = this._config.get('xps').api_key;
+    const customer_id = this._config.get('xps').customer_id;
+    const integration_id = this._config.get('xps').integration_id;
+    const url = `https://xpsshipper.com/restapi/v1/customers/${customer_id}/integrations/${integration_id}/orders/${payload.orderId}`;
 
-      const response = await axios.put(url, payload, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `RSIS ${apiKey}`,
-        },
-      });
-      console.log(response);
-      console.log(payload);
-      console.log(response.data);
+    const response = await axios.put(url, payload, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `RSIS ${apiKey}`,
+      },
+    });
+    console.log(response);
+    console.log(payload);
+    console.log(response.data);
 
-      return { data: response.data };
-    } catch (error) {
-      console.error(error.data);
-      return { error };
-    }
+    return { data: response };
   }
 
   async deleteOrder(job: Job): Promise<any> {
