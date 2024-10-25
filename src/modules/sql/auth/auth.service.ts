83bbf515dbeb5b1303625e54880a23feb52057aa
mj <jabir@newagesysit.com>
2024-10-25 12:46:37 +0530

    referral bug fix

--- src/modules/sql/auth/auth.service.ts
+++ src/modules/sql/auth/auth.service.ts
@@ -698,7 +698,7 @@ export class AuthService {
                 include: [
                   {
                     association: 'items',
-                    // include: [{ association: 'product' }],
+                    include: [{ association: 'product', required: true }],
                   },
                 ],
               },
