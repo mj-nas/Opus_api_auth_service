6da3cc5a9e036071572aa9645220a6f0f0ec1e9f
mj <jabir@newagesysit.com>
2024-11-11 20:13:23 +0530

    cart update validation

--- src/modules/sql/cart-item/cart-item.service.ts
+++ src/modules/sql/cart-item/cart-item.service.ts
@@ -1,4 +1,4 @@
-import { ModelService, SqlService } from '@core/sql';
+import { ModelService, SqlJob, SqlService } from '@core/sql';
 import { Injectable } from '@nestjs/common';
 import { CartItem } from './entities/cart-item.entity';
 
@@ -12,5 +12,36 @@ export class CartItemService extends ModelService<CartItem> {
 
   constructor(db: SqlService<CartItem>) {
     super(db);
+  }
+
+  /**
+   * doBeforeCreate
+   * @function function will execute before create function
+   * @param {object} job - mandatory - a job object representing the job information
+   * @return {void}
+   */
+  protected async doBeforeCreate(job: SqlJob<CartItem>): Promise<void> {
+    await super.doBeforeCreate(job);
+    const { cart_id, product_id } = job.payload;
+    const item = await this.$db.findOneRecord({
+      options: {
+        where: {
+          cart_id: cart_id,
+          product_id: product_id,
+        },
+      },
+    });
+
+    if (item.data) {
+      const newCart = await this.$db.updateRecord({
+        id: item.data.id,
+        body: {
+          quantity: item.data.quantity + 1,
+        },
+      });
+      if (!newCart.error) {
+        throw 'Cart updated Successfully';
+      }
+    }
   }
 }
